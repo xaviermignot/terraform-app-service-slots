223d994df0382e6c5645f842030f9907607a6357
Xavier Mignot <code@xmi.fr>
2022-11-10 22:27:47 -0500

    feat: Add some conf

--- infra/app_service/az-slot.tf
+++ infra/app_service/az-slot.tf
@@ -2,6 +2,10 @@ resource "azurerm_linux_web_app_slot" "staging" {
   name           = "staging"
   app_service_id = azurerm_linux_web_app.app.id
 
+  app_settings = {
+    someKey = "A new version"
+  }
+
   site_config {
     application_stack {
       dotnet_version = "6.0"
