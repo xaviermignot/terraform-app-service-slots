7cf4e359ce389935136dc21683e2a0c1d96ebef6
Xavier Mignot <code@xmi.fr>
2022-12-18 20:03:33 -0500

    chore: Remove sample Api app

--- src/DemoApi/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-var version = "0.1.0";
-var valueFromConf = app.Configuration["someKey"] ?? "none";
-
-app.MapGet("/", () => $"v{version}, value from configuration: {valueFromConf}");
-
-app.Run();
